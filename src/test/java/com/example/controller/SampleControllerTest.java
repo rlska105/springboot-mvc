cf937fe1d8597dc594c30bc484314578235a1592
Ginamkim <kimginam1995@gmail.com>
2020-03-25 14:11:27 +0900

    mockmvc test

--- src/test/java/com/example/controller/SampleControllerTest.java
+++ src/test/java/com/example/controller/SampleControllerTest.java
@@ -1,12 +1,34 @@
 package com.example.controller;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class SampleControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public void sample() {
+    public void sample() throws Exception {
+        String sample = "sample";
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(sample))
+                .andDo(print());
     }
 }

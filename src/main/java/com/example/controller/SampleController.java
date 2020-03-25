c2354c822897cd1f638c896fee14ad9f7411e680
Ginamkim <kimginam1995@gmail.com>
2020-03-25 12:36:07 +0900

    initial

---
+++ src/main/java/com/example/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/")
+    public String sample(){
+        return "sample";
+    }
+}
---
+++ src/test/java/com/example/controller/SampleControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SampleControllerTest {
+
+    @Test
+    public void sample() {
+    }
+}

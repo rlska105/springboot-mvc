e6f4945ea9d86574f0307392a6e6f6e34cbb0dca
Ginamkim <kimginam1995@gmail.com>
2020-04-11 20:00:41 +0900

    update css적용 전

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,12 +1,14 @@
 package com.example.controller;
 
 
+import com.example.controller.dto.WordsResponseDto;
 import com.example.service.words.WordsService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -20,5 +22,12 @@ public class MainController {
     public String main(Model model) {
         model.addAttribute("words", wordsService.findAllDesc());
         return "main";
+    }
+
+    @GetMapping("/words/update/{id}")
+    public String update(@PathVariable Long id, Model model){
+        WordsResponseDto dto = wordsService.findById(id);
+        model.addAttribute("post", dto);
+        return "words-update";
     }
 }
